de1d31a3c93bcc3e660a7181f7ec5ac996b4a4fb
Yan Pablo <93152439+YanppSilva@users.noreply.github.com>
2024-01-15 14:45:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const chatInput = document.querySelector('.chat-input textarea');
 const sendChatBtn = document.querySelector('.chat-input span');
 const chatbox = document.querySelector('.chatbox');
+const chatBotToggler = document.querySelector('.chatbot-toggler');
 
 let userMessage;
 const API_KEY = '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';
@@ -9,9 +10,10 @@ const createChatLi = (message, className) => {
   // Cria um elemento de chat <li> com a menssagem passada e a classe.
   const chatLi = document.createElement('li');
   chatLi.classList.add('chat', className);
-  let chatContent = className === 'outgoing' ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+  let chatContent = className === 'outgoing' ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
   chatLi.innerHTML = chatContent;
-  return chatLi
+  chatLi.querySelector('p').textContent = message;
+  return chatLi;
 }
 
 const generateResponse = (incomingChatLi) => {
@@ -22,7 +24,7 @@ const generateResponse = (incomingChatLi) => {
     method: 'POST',
     headers: {
       'Content-Type': 'aplication/json',
-      'Authorization': `Bearer${API_KEY}`
+      'Authorization': `Bearer ${API_KEY}`
     },
     body: JSON.stringify({
       model: 'gpt-3.5-turbo',
@@ -34,22 +36,25 @@ const generateResponse = (incomingChatLi) => {
     messageElement.textContent = data.choices[0].message.content;
   }).catch((error) => {
     messageElement.textContent = 'Oops! Algo deu errado. Tente novamente';
-  })
+  }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
 }
 
 const handleChat = () => {
   userMessage = chatInput.value.trim();
   if(!userMessage) return;
+  chatInput.value = '';
   
   // Anexa a mensagem do usuário à caixa de bate-papo
   chatbox.appendChild(createChatLi(userMessage, 'outgoing'));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
 
   setTimeout(() => {
     // Mostra a mensagem "Processando..." enquanto espera pela resposta do bot.
     const incomingChatLi = createChatLi('Processando...', 'incoming')
     chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
     generateResponse(incomingChatLi);
   }, 600);
 }
-
 sendChatBtn.addEventListener('click', handleChat);
+chatBotToggler.addEventListener('click', () => document.body.classList.toggle('show-chatbot'));
